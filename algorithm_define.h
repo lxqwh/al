4e245df39d40a5a8868f46dbbda6db7cff6a4740
lxqwh <605972145@qq.com>
2015-07-28 23:23:59 +0800

    7.28

--- algorithm_define.h
+++ algorithm_define.h
@@ -4,45 +4,94 @@
 #include "graph_t.h"
 #include <string>
 
+
+#define BL_ 30
+#define LUT_ 163
+#define MAS_ 356
+#define SH_ 164
+#define MM_ 462
+#define GFM_ 422
+#define SUB_ 60
+
+
+
+
 #define IDEA(g)                                                                                           \
-	(g).Insertedge(1,2,462);   (g).Insertedge(1,3,356);   (g).Insertedge(1,4,356); (g).Insertedge(1,4,462);       \
+	(g).Insertedge(1,2,MM_);   (g).Insertedge(1,3,MAS_);   (g).Insertedge(1,4,MAS_); (g).Insertedge(1,4,MM_);       \
 	                                                                                                      \
 	(g).Insertedge(2,18,0);    (g).Insertedge(2,6,0);     (g).Insertedge(3,22,0);   (g).Insertedge(3,8,0);        \
 	(g).Insertedge(4,6,0);     (g).Insertedge(4,16,0);    (g).Insertedge(5,8,0);    (g).Insertedge(5,20,0);       \
 	                                                                                                      \
-	(g).Insertedge(6,7,30);    (g).Insertedge(8,9,30);                                                        \
+	(g).Insertedge(6,7,BL_);    (g).Insertedge(8,9,BL_);                                                        \
 	                                                                                                      \
-	(g).Insertedge(7,10,462);  (g).Insertedge(9,11,0);                                                        \
+	(g).Insertedge(7,10,MM_);  (g).Insertedge(9,11,0);                                                        \
 	                                                                                                      \
 	(g).Insertedge(10,14,0);   (g).Insertedge(10,11,0);                                                       \
 	                                                                                                      \
-	(g).Insertedge(11,12,356);                                                                              \
+	(g).Insertedge(11,12,MAS_);                                                                              \
 	                                                                                                      \
-	(g).Insertedge(12,13,462);                                                                              \
+	(g).Insertedge(12,13,MM_);                                                                              \
 	                                                                                                      \
 	(g).Insertedge(13,18,0) ;  (g).Insertedge(13,14,0);   (g).Insertedge(13,16,0);                              \
 	                                                                                                      \
-	(g).Insertedge(18,19,30);	 (g).Insertedge(14,15,356); (g).Insertedge(16,17,30);                             \
+	(g).Insertedge(18,19,BL_);	 (g).Insertedge(14,15,MAS_); (g).Insertedge(16,17,BL_);                             \
 	                                                                                                      \
 	(g).Insertedge(15,22,0);   (g).Insertedge(15,20,0);                                                       \
 	                                                                                                      \
-	(g).Insertedge(22,23,30);  (g).Insertedge(20,21,30); 
+	(g).Insertedge(22,23,BL_);  (g).Insertedge(20,21,BL_); 
 
 
 #define AES(g)                  \
-    (g).Insertedge(1,2,163);      \
-	(g).Insertedge(2,3,164);      \
-	(g).Insertedge(3,4,422);      \
-	(g).Insertedge(4,5,30);
+    (g).Insertedge(1,2,LUT_);      \
+	(g).Insertedge(2,3,SH_);      \
+	(g).Insertedge(3,4,GFM_);      \
+	(g).Insertedge(4,5,BL_);
 	
+
 #define DES(g)                  \
-    (g).Insertedge(1,2,60);       \
+    (g).Insertedge(1,2,SUB_);       \
     (g).Insertedge(1,6,0);	    \
-	(g).Insertedge(2,3,30);       \
-	(g).Insertedge(3,4,163);      \
-	(g).Insertedge(4,5,60);       \
+	(g).Insertedge(2,3,BL_);       \
+	(g).Insertedge(3,4,LUT_);      \
+	(g).Insertedge(4,5,SUB_);       \
 	(g).Insertedge(5,6,0);	    \
-	(g).Insertedge(6,7,30);		
+	(g).Insertedge(6,7,BL_);		
+	
+	
+	
+#define BLOWFISH(g)                  \
+    (g).Insertedge(1,2,BL_);       \
+	                                                                                      \
+	(g).Insertedge(2,3,LUT_);     (g).Insertedge(2,4,LUT_);  (g).Insertedge(2,5,LUT_);    \
+	(g).Insertedge(2,6,LUT_);  	                                                          \
+	                                                                                      \
+    (g).Insertedge(3,7,0);   (g).Insertedge(4,7,0);                                       \
+	                                                                                      \
+	(g).Insertedge(7,8,MAS_);                                                             \
+	                                                                                      \
+	(g).Insertedge(8,9,0);      (g).Insertedge(5,9,0);                                    \
+	                                                                                      \
+	(g).Insertedge(9,10,BL_);                                                             \
+	                                                                                      \
+	(g).Insertedge(10,11,0);	  (g).Insertedge(6,11,0);                                 \
+	                                                                                      \
+	(g).Insertedge(11,12,MAS_);		                                                      \
+                                                                                          \
+	(g).Insertedge(12,13,0);(g).Insertedge(1,13,0);                                       \
+                                                                                          \
+    (g).Insertedge(13,14,BL_);
+
+
+#define CAMELLIA(g)                  \
+    (g).Insertedge(1,2,LUT_);       \
+	(g).Insertedge(2,3,BL_);       \
+	(g).Insertedge(3,4,BL_);       \
+	(g).Insertedge(4,5,BL_);       \
+	(g).Insertedge(5,6,BL_);       \
+	(g).Insertedge(6,7,SUB_);       \
+	(g).Insertedge(7,8,BL_);	
+	
+
 	
 inline void	init_al_graph(ALGraph<int> &g, std::string al_type){
 	if(al_type == "IDEA" ){
@@ -62,8 +111,23 @@ inline void	init_al_graph(ALGraph<int> &g, std::string al_type){
 	    }
 		DES(g);  
 	}
+	else if(al_type == "BLOWFISH"){  
+		for(int i = 0; i <= 13; i++){
+		g.InsertVertex(i+1);
+	    }
+		BLOWFISH(g);  
+	}	
+	else if(al_type == "CAMELLIA"){  
+		for(int i = 0; i <= 7; i++){
+		g.InsertVertex(i+1);
+	    }
+		CAMELLIA(g);  
+	}	
 }
 
 
-	#endif
+
+
+
+#endif
 
--- test.cc
+++ test.cc
@@ -2,26 +2,33 @@
 #include "algorithm_define.h"
 #include "al_base.h"
 #include <iostream>
+#include <fstream>
 
 using std::cout;
 using std::endl;
 
+
+std::ofstream out("output.txt");
+
+
 void print_element(int i){
 	switch(i){
-	case 30:     cout<<"logic"; break;
-	case 163:    cout<<"LUT"; break;
-	case 356:    cout<<"Modular add or sub "; break;
-	case 164:    cout<<"Shift"; break;
-	case 462:    cout<<"Modular Multiplication"; break;
-	case 422:    cout<<"GF Multiplication"; break;
-	case 60:     cout<<"substitution"; break;
-	default:     cout<<"error elememt,the delay is:"<<i<<endl; break;
+	case 30:     out<<"logic"; break;
+	case 163:    out<<"LUT"; break;
+	case 356:    out<<"Modular add or sub "; break;
+	case 164:    out<<"Shift"; break;
+	case 462:    out<<"Modular Multiplication"; break;
+	case 422:    out<<"GF Multiplication"; break;
+	case 60:     out<<"substitution"; break;
+	default:     out<<"error elememt,the delay is:"<<i<<endl; break;
 	}
 }
 
 int main(){
 
+
 //*************AES******************	
+	out << "*************AES***************" << endl;
 	vector<al_protocol::element> ie_aes = {al_protocol::LUT,al_protocol::SH,
 	                                       al_protocol::GFM,al_protocol::BL};
 	//al_base aes = al_base(10, 32, 5, ie_aes);//初始化还是赋值，这里是调用拷贝初始化？？？？
@@ -30,18 +37,19 @@ int main(){
 	std::vector<al_protocol::element> aes_r;
 	int aes_delay;
 	aes.key_path(aes_r,aes_delay);
-	cout<<"the critical path of aes is:"<<endl;
+	out<<"the critical path of aes is:"<<endl;
 	for(al_protocol::element i:aes_r){
 		print_element(i);
-		cout<<" -> "<<endl;		
+		out<<" -> "<<endl;		
 	}
-	cout<<"the total delay of a round is :"<<aes_delay/100.0<<"ns"<<endl;	
+	out<<"the total delay of a round is :"<<aes_delay/100.0<<"ns"<<endl;	
 	al_protocol::element aes_maxe;
 	aes.max_delay_element(aes_maxe);
-	cout<<"the max_delay_element is :"<<aes_maxe/100.0<<"ns"<<endl;	
+	out<<"the max_delay_element is :"<<aes_maxe/100.0<<"ns"<<endl;	
 
 
 //*************DES*******************
+	out << endl<<"*************DES****************" << endl;
     vector<al_protocol::element> ie_des = {al_protocol::SUB, al_protocol::BL,
 	                                       al_protocol::LUT};
 	al_base des(16,32,7,ie_des);
@@ -49,17 +57,18 @@ int main(){
 	std::vector<al_protocol::element> des_r;
 	int des_delay;
 	des.key_path(des_r,des_delay);
-	cout<<"the critical path of des is:"<<endl;
+	out<<"the critical path of des is:"<<endl;
 	for(al_protocol::element i:des_r){
 		print_element(i);
-		cout<<" -> "<<endl;		
+		out<<" -> "<<endl;		
 	}
-	cout<<"the total delay of a round is :"<<des_delay/100.0<<"ns"<<endl;	
+	out<<"the total delay of a round is :"<<des_delay/100.0<<"ns"<<endl;	
 	al_protocol::element des_maxe;
 	des.max_delay_element(des_maxe);
-	cout<<"the max_delay_element is :"<<des_maxe/100.0<<"ns"<<endl;	
+	out<<"the max_delay_element is :"<<des_maxe/100.0<<"ns"<<endl;	
 
 //**************IDEA******************
+	out << endl << "*************IDEA*************" << endl;
     vector<al_protocol::element> ie_idea = {al_protocol::MM, al_protocol::MAS,
 	                                        al_protocol::BL};
 	al_base idea(8,16,23,ie_idea);
@@ -67,16 +76,54 @@ int main(){
 	std::vector<al_protocol::element> idea_r;
 	int idea_delay;
 	idea.key_path(idea_r,idea_delay);
-	cout<<"the critical path of idea is:"<<endl;
+	out<<"the critical path of idea is:"<<endl;
 	for(al_protocol::element i:idea_r){
 		print_element(i);
-		cout<<" -> "<<endl;		
+		out<<" -> "<<endl;		
 	}
-	cout<<"the total delay of a round is :"<<idea_delay/100.0<<"ns"<<endl;	
+	out<<"the total delay of a round is :"<<idea_delay/100.0<<"ns"<<endl;	
 	al_protocol::element idea_maxe;
 	idea.max_delay_element(idea_maxe);
-	cout<<"the max_delay_element is :"<<idea_maxe/100.0<<"ns"<<endl;	
+	out<<"the max_delay_element is :"<<idea_maxe/100.0<<"ns"<<endl;	
 	
+//**************BLOWFISH******************
+	out << endl << "*************BLOWFISH*************" << endl;
+    vector<al_protocol::element> ie_blowfish = {al_protocol::LUT, al_protocol::BL,
+	                                        al_protocol::MAS};
+	al_base blowfish(16,32,14,ie_blowfish);
+	blowfish.init_graph(init_al_graph,"BLOWFISH");
+	std::vector<al_protocol::element> blowfish_r;
+	int blowfish_delay;
+	blowfish.key_path(blowfish_r,blowfish_delay);
+	out<<"the critical path of blowfish is:"<<endl;
+	for(al_protocol::element i:blowfish_r){
+		print_element(i);
+		out<<" -> "<<endl;		
+	}
+	out<<"the total delay of a round is :"<<blowfish_delay/100.0<<"ns"<<endl;	
+	al_protocol::element blowfish_maxe;
+	blowfish.max_delay_element(blowfish_maxe);
+	out<<"the max_delay_element is :"<<blowfish_maxe/100.0<<"ns"<<endl;		
+	
+	
+//**************CAMELLIA******************
+	out << endl << "*************CAMELLIA*************" << endl;
+    vector<al_protocol::element> ie_camellia = {al_protocol::LUT, al_protocol::BL,
+	                                            al_protocol::SUB};
+	al_base camellia(18,8,8,ie_camellia);
+	camellia.init_graph(init_al_graph,"CAMELLIA");
+	std::vector<al_protocol::element> camellia_r;
+	int camellia_delay;
+	camellia.key_path(camellia_r,camellia_delay);
+	out<<"the critical path of camellia is:"<<endl;
+	for(al_protocol::element i:camellia_r){
+		print_element(i);
+		out<<" -> "<<endl;		
+	}
+	out<<"the total delay of a round is :"<<camellia_delay/100.0<<"ns"<<endl;	
+	al_protocol::element camellia_maxe;
+	camellia.max_delay_element(camellia_maxe);
+	out<<"the max_delay_element is :"<<camellia_maxe/100.0<<"ns"<<endl;		
 	
 	return 0;
 	
